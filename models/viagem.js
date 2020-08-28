311099055edaa0c69dd2e6744d7a529783084ad9
unknown <lhgoulart92@gmail.com>
2020-08-27 22:50:06 -0300

    ...

--- models/viagem.js
+++ models/viagem.js
@@ -1,13 +1,13 @@
 var mongoose = require("mongoose");
 
 var viagemSchema = new mongoose.Schema({
-	name: String,
-	phone: String,
-	dateFrom: Date,
-	dateTo: Date,
-	origin: String,
-	destination: String,
-	people: Number
+	name: {type: String, required: [true, 'Name required']},
+	phone: {type: String, required: [true, 'Phone required']},
+	dateFrom: {type: Date, required: [true, 'Check-in date required']},
+	dateTo: {type: Date, required: [true, 'Check-out date required']},
+	origin: {type: String, required: [true, 'Origin required']},
+	destination: {type: String, required: [true, 'Destination required']},
+	people: {type: Number, required: [true, 'Number of travelers required'], min: 1}
 });
 
 module.exports = mongoose.model("Viagem", viagemSchema);
